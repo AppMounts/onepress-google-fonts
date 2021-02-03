b9c625ff180400088aa4f9ea34eddd19688b3b6f
Sergey Fritzler <sergey@fritzler.it>
2021-02-03 21:02:48 +0100

    first version

---
+++ onepress-google-fonts.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: OnePress Google Fonts
+ * Description: Privacy compliant solution for Google Fonts
+ *
+ * Version: 1.0
+ * Author: Sergey Fritzler
+ * Author URI: http://appmounts.de/
+ * License: Copyright by Sergey Fritzler
+ */
+
+define('GDPR_GF_VERSION', 1.0);
+
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style('onepress-google-fonts',  plugins_url('fonts/fonts.css', __FILE__), [], GDPR_GF_VERSION);
+});
